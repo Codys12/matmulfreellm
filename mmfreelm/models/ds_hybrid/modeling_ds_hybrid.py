81522462b71fc8970175905b82c8336bf5b1bff5
Codys12 <41397239+Codys12@users.noreply.github.com>
2024-07-27 09:30:20 -0500

    debug

--- mmfreelm/models/ds_hybrid/modeling_ds_hybrid.py
+++ mmfreelm/models/ds_hybrid/modeling_ds_hybrid.py
@@ -648,8 +648,11 @@ class DSHybridForCausalLM(DSHybridBitPreTrainedModel):
             all_logits.append(logits.detach())
 
         grad_output = detached_hidden_states.grad
+        print("GRAD OUTPUT: \n")
         print(grad_output)
         total_loss = torch.sum(hidden_states * grad_output)
+        print("LOSS: \n")
+        print(total_loss)
 
         # Stack all logits after the loop
         logits = torch.stack(all_logits, dim=1)

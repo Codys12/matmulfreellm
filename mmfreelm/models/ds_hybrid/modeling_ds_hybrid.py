57cb8721d6307688401ad3b02eb227bd96f80db4
Codys12 <41397239+Codys12@users.noreply.github.com>
2024-08-16 11:00:09 -0500

    fix soft_loss

--- mmfreelm/models/ds_hybrid/modeling_ds_hybrid.py
+++ mmfreelm/models/ds_hybrid/modeling_ds_hybrid.py
@@ -776,7 +776,7 @@ class DSHybridForCausalLM(DSHybridBitPreTrainedModel):
                 head_loss = hard_loss_fct(logits.view(-1, self.config.vocab_size), labels.view(-1))
 
             if soft_targets is not None:
-                soft_targets = torch.cat((soft_targets[..., 1:, :], torch.full_like(soft_targets[..., :1, :], loss_fct.ignore_index)), 1)
+                soft_targets = torch.cat((soft_targets[..., 1:, :], torch.full_like(soft_targets[..., :1, :], soft_loss_fct.ignore_index)), 1)
                 soft_loss = soft_loss_func(logits, soft_targets)
                 head_loss = soft_loss if head_loss is None else head_loss + soft_loss
 
